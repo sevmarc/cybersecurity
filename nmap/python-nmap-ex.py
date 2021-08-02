131fb589cf51ecf36b43a76a17aa54eee4ece50f
sevmarc <marton.sevella@gmail.com>
2021-08-02 22:12:40 +0200

    nmap init upload, minimal content, similar usage as to cli

---
+++ nmap/python-nmap-ex.py
@@ -0,0 +1,11 @@
+import nmap
+import os
+import sys
+
+nm = nmap.PortScanner()
+
+sc = nm.scan('127.0.0.1', '22-443')
+print(sc)
+print(nm.command_line())
+print(nm.scaninfo())
+print(nm.all_hosts())
