69ba2f3d8b3ed0aaf4ecd61e20cb827abc2238df
sevmarc <marton.sevella@gmail.com>
2021-08-14 20:40:19 +0200

    caesar shift cypher added

---
+++ cryptography/caesar.py
@@ -0,0 +1,19 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def caesar_cypher(message: str, shift_value: int, abc:str=alphabet) -> str:
+    # ord('a') = 97; ord('z) = 122; 
+    # ord('A') = 65; ord('Z') = 90
+    message_lower = message.lower()  # capitalization is lost, don't care
+    
+    message_decoded = ''
+    for l in message_lower:
+        index_ = alphabet.find(l)
+        shifted_index = (index_ + shift_value) % len(alphabet)
+        message_decoded += alphabet[shifted_index]
+    return message_decoded
+
+
+if __name__ == '__main__':
+    print(caesar_cypher('abcdefghijklmnopqrstuvwxyz', 5))
+    # decoding done with negative shift value
+    print(caesar_cypher('fghijklmnopqrstuvwxyzabcde', -5)) 
