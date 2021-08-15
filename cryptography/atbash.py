94cfc6edbf1b3207788158feb77fbf9f974c3f5c
sevmarc <marton.sevella@gmail.com>
2021-08-15 14:21:13 +0200

    atbash cypher implemented - reversed alphabet cipher used in hebrew languages

---
+++ cryptography/atbash.py
@@ -0,0 +1,17 @@
+from alphabet import english_alphabet as alphabet
+
+
+def atbash_cypher(message: str, abc:str=alphabet) -> str:
+    message_lower = message.lower()  # capitalization is lost, don't care
+    
+    message_decoded = ''
+    for l in message_lower:
+        index_ = alphabet.find(l)
+        message_decoded += list(reversed(alphabet))[index_]
+    return message_decoded
+
+
+if __name__ == '__main__':
+    print(atbash_cypher('abcdefghijklmnopqrstuvwxyz')) 
+    # applying it twice deciphers
+    print(atbash_cypher(atbash_cypher('abcdefghijklmnopqrstuvwxyz')))
