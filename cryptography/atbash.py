24a10e982c005729f2022603beb0ed23abf7d976
sevmarc <marton.sevella@gmail.com>
2021-08-15 15:09:10 +0200

    handling characters missing from alphabet (by skipping/removing them)

--- cryptography/atbash.py
+++ cryptography/atbash.py
@@ -7,6 +7,8 @@ def atbash_cypher(message: str, abc:str=alphabet) -> str:
     message_decoded = ''
     for l in message_lower:
         index_ = alphabet.find(l)
+        if index_ == -1:  # not found char in alphabet
+            continue  # we skip/remove these characters
         message_decoded += list(reversed(alphabet))[index_]
     return message_decoded
 
--- cryptography/caesar.py
+++ cryptography/caesar.py
@@ -9,12 +9,14 @@ def caesar_cypher(message: str, shift_value: int, abc:str=alphabet) -> str:
     message_decoded = ''
     for l in message_lower:
         index_ = alphabet.find(l)
+        if index_ == -1:  # not found char in alphabet
+            continue  # we skip/remove these characters
         shifted_index = (index_ + shift_value) % len(alphabet)
         message_decoded += alphabet[shifted_index]
     return message_decoded
 
 
 if __name__ == '__main__':
-    print(caesar_cypher('abcdefghijklmnopqrstuvwxyz', 5))
+    print(caesar_cypher('abcdefghijklm -------124nopqrstuvwxyz', 5))
     # decoding done with negative shift value
     print(caesar_cypher('fghijklmnopqrstuvwxyzabcde', -5)) 
