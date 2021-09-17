c0e713f0bbd78f912a46158b7875dd922ed529a8
sevmarc <marton.sevella@gmail.com>
2021-09-17 14:09:14 +0200

    cryptoanalysis updated, keyword lookup implemented (english)

--- cryptography/cryptanalysis.py
+++ cryptography/cryptanalysis.py
@@ -1,8 +1,8 @@
-from os import device_encoding
 import pandas
 from collections import Counter
 from math import sqrt
 from alphabet import english_alphabet as alphabet
+from alphabet import english_keywordlist as keywords
 
 from caesar import caesar_cypher
 from atbash import atbash_cypher
@@ -18,7 +18,7 @@ known_ciphers_codes = {
     # method: keyrange
     caesar_cypher: range(1, len(alphabet)),
     atbash_cypher: None,
-    rot: range(1, 10),  # this range is arbitrary (ascii)
+    rot: range(1, 50),  # this range is arbitrary (ascii)
 }
 
 
@@ -40,6 +40,10 @@ def calculate_deviance(message: str,
                 continue
             else:
                 deviance += 100  # character not in alphabet
+    # the keyword lookup might need calibration, good first solution for now
+    for word_ in message_low.split():
+        if word_ in keywords:
+            deviance /= 2
     return deviance / (message_len ** 2)
 
 
@@ -53,8 +57,11 @@ class CipherCode:
         for method, keyrange in known_ciphers_codes.items():
             if keyrange:
                 for key_ in keyrange:
-                    decoded_message = method(self.coded_message, key_, reverse=True)
-                    self.decoded_dict.update( { str(method) + str(key_): decoded_message})
+                    try:
+                        decoded_message = method(self.coded_message, key_, reverse=True)
+                        self.decoded_dict.update( { str(method) + str(key_): decoded_message})
+                    except ValueError:
+                        continue
             else:
                 decoded_message = method(self.coded_message)
                 self.decoded_dict.update( {method: decoded_message})
@@ -70,14 +77,7 @@ class CipherCode:
 
 
 if __name__ == '__main__':
-    example_message = """This is some of the example text, this next
-    part is also still the message. It will be passed on to the 
-    cipher, and we will try to decode using letter frequency. """
-
-    example_key = 20
-    possible_keys = list(range(len(alphabet)))
-
-    coded_message = caesar_cypher(example_message, example_key)
+    coded_message = "Gur cnffjbeq vf 5Gr8L4qetPEsPk8htqjhRK8XSP6x2RHh"
 
     trial = CipherCode(coded_message)
     trial.create_decode_dict()
