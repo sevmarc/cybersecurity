5fd0fd0df8f47ab16b91cd49bbb91b009bbbac93
sevmarc <marton.sevella@gmail.com>
2021-09-17 08:52:28 +0200

    caeasr cypher properly handles upper- and lower-case letters

--- cryptography/caesar.py
+++ cryptography/caesar.py
@@ -7,23 +7,24 @@ def caesar_cypher(message: str,
                   reverse: bool = False) -> str:
     # ord('a') = 97; ord('z) = 122; 
     # ord('A') = 65; ord('Z') = 90
-    message_lower = message.lower()  # capitalization is lost, don't care
     if reverse:
         shift_value *= -1
 
     message_decoded = ''
-    for l in message_lower:
-        index_ = alphabet.find(l)
+    for l in message:
+        if l.islower():
+            abc = abc.lower()
+        elif l.isupper():
+            abc = abc.upper()
+        index_ = abc.find(l)
         if index_ == -1:  # not found char in alphabet
             # can be removed, or left as original
             message_decoded += l
             continue  # we skip/remove these characters
-        shifted_index = (index_ + shift_value) % len(alphabet)
-        message_decoded += alphabet[shifted_index]
+        shifted_index = (index_ + shift_value) % len(abc)
+        message_decoded += abc[shifted_index]
     return message_decoded
 
 
 if __name__ == '__main__':
-    print(caesar_cypher('abcdefghijklm -------124nopqrstuvwxyz', 5))
-    # decoding done with negative shift value
-    print(caesar_cypher('fghijklmnopqrstuvwxyzabcde', -5)) 
+    print(caesar_cypher('Gur cnffjbeq vf 5Gr8L4qetPEsPk8htqjhRK8XSP6x2RHh', 13, reverse=True))
